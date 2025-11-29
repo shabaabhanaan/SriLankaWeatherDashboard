7c0eab77311005f7b8fbc2b556c7436c99d4ee40
Shabaab Hanaan <shababhanaan22@gmail.com>
2025-11-29 06:22:25 +0530

    Update API key in news.js
    Replaced API key environment variable with a hardcoded value.

--- news.js
+++ news.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
-    const API_KEY = process.env.WORLDNEWS_API_KEY;
+    const API_KEY = process.env.3491013404034ab1b5bfead1c09ffb2a;
 
     const url = `https://api.worldnewsapi.com/search-news?source-country=lk&api-key=${API_KEY}`;
 
